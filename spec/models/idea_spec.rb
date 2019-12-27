8a7da3797fdbe44bc4c0c04d031b75013c27c187
youichi yamazaki <denncoru@yahoo.co.jp>
2019-12-27 16:01:06 +0900

    spec/modesにテストを追加

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
+require 'support/utilities'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: '成功テスト', email: 'success@yahoo.co.jp', password: 'testtest')
+    %W[フィギュア キーホルダー・ストラップ 缶バッジ 雑貨 アパレルアイテム アクセサリー コスメ関連 文具デスク用品 その他].each { |sample| Category.create(name: sample) }
+  end
+
+
+  it "titleが空ならバリデーションが通らない" do
+    idea = Idea.new(title: '', content: '失敗テスト', category_ids: '1', tag_list: '鬼滅の刃', user: @user)
+    expect(idea).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    idea = Idea.new(title: '失敗テスト', content: '', category_ids: '1', tag_list: '鬼滅の刃', user: @user)
+    expect(idea).not_to be_valid
+  end
+
+  it "category_idsが空ならバリデーションが通らない" do
+    idea = Idea.new(title: '失敗テスト', content: '失敗テスト', category_ids: '', tag_list: '鬼滅の刃', user: @user)
+    expect(idea).not_to be_valid
+  end
+
+  it "tag_listが空ならバリデーションが通らない" do
+    idea = Idea.new(title: '失敗テスト', content: '失敗テスト', category_ids: '1', tag_list: '', user: @user)
+    expect(idea).not_to be_valid
+  end
+
+  it "全ての値が入力されていればバリデーションが通る" do
+    idea = Idea.new(title: '失敗テスト', content: '失敗テスト', category_ids: '1', tag_list: '鬼滅の刃', user: @user)
+    expect(idea).to be_valid
+  end
 end

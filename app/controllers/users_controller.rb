f105b4ea55cf43304fa514b705d99c9aa7dd7622
youichi yamazaki <denncoru@yahoo.co.jp>
2019-12-27 17:46:02 +0900

    userのcontroller修正,辞書機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show, :edit, :update]
   before_action :authenticate_user!, only:[:edit, :update, :destory]
+  before_action :check_user, only: [:edit, :update, :delete]
 
   def show
   end
@@ -34,5 +35,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :image, :profile, :profile_image_cache, :email, :profile_image)
+  end
+
+  def check_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+        redirect_to top_ideas_path,notice:"権限がありません"
+    end
   end
 end

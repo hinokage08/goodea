746bc0e56fcf41d5fd7d7185d72aadb293f7f73b
youichi yamazaki <denncoru@yahoo.co.jp>
2020-06-26 20:33:34 +0900

    job修正

--- app/jobs/tag_image_job.rb
+++ app/jobs/tag_image_job.rb
@@ -4,7 +4,7 @@ class TagImageJob < ApplicationJob
   def perform
     ActsAsTaggableOn::Tag.most_used.first(5).each do |tag|
       tag_name = tag.name
-      unless tag.image == nil
+      if tag.image == nil
         if @tag = ActsAsTaggableOn::Tag.find_by(name: tag_name)
           anime_img = Trend.anict_search_image(tag_name)
           anime_img = anime_img.delete("[]") if anime_img.empty?

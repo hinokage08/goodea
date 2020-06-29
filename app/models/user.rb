e6a7290ffa8946951697f50b87da1e5a0eeb1c3f
youichi yamazaki <denncoru@yahoo.co.jp>
2020-06-29 14:33:32 +0900

    簡単ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,12 @@ class User < ApplicationRecord
         def password_required?
           super && provider.blank?
         end
+
+        def self.guest
+          find_or_create_by(name: 'guest', email: 'guest@example.com') do |user|
+            user.password = SecureRandom.urlsafe_base64
+          end
+        end
       
         private
       
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'}
+
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    end
   resources :users do
     member do
       get :following, :followers

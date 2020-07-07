2c97d821dc1115bd853805c0a5c25fe97cbcb44f
youichi yamazaki <denncoru@yahoo.co.jp>
2020-07-07 19:44:21 +0900

    アクセス制限機能を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show, :edit, :update]
   before_action :authenticate_user!, only:[:edit, :update, :destory]
-  before_action :check_user, only: [:edit, :update, :delete]
+  before_action :check_user, only: [:edit, :update, :delete, :following, :followers]
 
   def show
   end

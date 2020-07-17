b279459f6302c97fcb469fdd460c5513a47e5c3b
youichi yamazaki <denncoru@yahoo.co.jp>
2020-07-17 12:59:53 +0900

    rspec追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
     before_action :authenticate_user!
     def create
         favorite = current_user.favorites.create(idea_id: params[:idea_id])
-        redirect_to ideas_url, notice: "#{favorite.idea.user.name}さんのアイデアをお気に入り登録しました"
+        redirect_to idea_path(favorite.idea_id), notice: "#{favorite.idea.user.name}さんのアイデアをお気に入りしました"
     end
     
     def destroy
@@ -11,10 +11,10 @@ class FavoritesController < ApplicationController
         favorites = favorites.map do |favorite|
           favorite.idea.user.name
         end
-        redirect_to favorite_user_path(current_user.id), notice: "#アイデアのいいねを解除しました"
+        redirect_to favorite_user_path(current_user.id), notice: "#アイデアのお気に入りを解除しました"
       else
         favorite = current_user.favorites.find_by(id: params[:id]).destroy
-        redirect_to idea_path(favorite.idea_id), notice: "アイデアのいいねを解除しました"
+        redirect_to idea_path(favorite.idea_id), notice: "アイデアのお気に入りを解除しました"
       end
     end
 end
